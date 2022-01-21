29668a322302a85b63feb81557e5bce7d5b6c443
KV-00 <vaillancourtkeven@gmail.com>
2022-01-20 23:33:04 -0500

    finished program

--- wheres-sausage-dog/js/script.js
+++ wheres-sausage-dog/js/script.js
@@ -6,17 +6,21 @@ const NUM_ANIMALS = 100;
 let animalImages = [];
 let animals = [];
 
+let sausageDogImage = undefined;
+let sausageDog = undefined;
+
 function preload() {
   for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
-    let animalImage = loadImage('assets/images/animal${i}.png');
+    let animalImage =  loadImage(`assets/images/animal${i}.png`);
     animalImages.push(animalImage);
   }
+
+  sausageDogImage = loadImage(`assets/images/sausage-dog.png`);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  // Create the animals
   for (let i = 0; i < NUM_ANIMALS; i++) {
     let x = random(0, width);
     let y = random(0, height);
@@ -24,6 +28,10 @@ function setup() {
     let animal = new Animal(x, y, animalImage);
     animals.push(animal);
   }
+
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImage);
 }
 
 function draw() {
@@ -32,4 +40,10 @@ function draw() {
   for (let i = 0; i < animals.length; i++) {
     animals[i].update();
   }
+
+  sausageDog.update();
+}
+
+function mousePressed() {
+  sausageDog.mousePressed();
 }
