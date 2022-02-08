1f17e24d9dcb650a1151fee8bbb33df177c17b8c
KV-00 <vaillancourtkeven@gmail.com>
2022-02-08 16:02:30 -0500

    Working on exercise

--- bubble-popper-plus-plus/js/script.js
+++ bubble-popper-plus-plus/js/script.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 // The user's webcam
@@ -9,43 +8,63 @@ let handpose = undefined;
 let predictions = [];
 // The bubble
 let bubble = undefined;
+let bubblesPopped = 0;
 
-function preload() {
+let sheep;
 
-}
+function preload() {}
 
 function setup() {
   createCanvas(640, 480);
 
+  sheep = loadImage(`assets/images/sheep0.png`);
+
   // Access user's webcam
-  video = createCapture(VIDEO);
-  video.hide();
+  //video = createCapture(VIDEO);
+  //video.hide();
 
   // Load the handpose model
-  handpose = ml5.handpose(video, {
-    flipHorizontal: true
-  }, function() {
-    console.log(`Model loaded.`);
-  });
+  handpose = ml5.handpose(
+    video,
+    {
+      flipHorizontal: true,
+    },
+    function () {
+      console.log(`Model loaded.`);
+    }
+  );
 
   // Listen for predictions
   handpose.on(`predict`, function (results) {
-    console.log(results);
+    //console.log(results);
     predictions = results;
   });
 
   // Our bubble
   bubble = {
-    x: random(width),
-    y: height,
+    scared: false,
+    x: width / 2,
+    y: height / 2,
     size: 100,
-    vx: 0,
-    vy: -2
+    vx: random(1, -1),
+    vy: random(1, -1),
+    r: 25,
+    angle: 0,
+    vxRunning: random(40, 50),
+    vyRunning: random(40, 50),
   };
 }
 
 function draw() {
-  background(0);
+  background(0, 255, 0);
+
+  // Bubbles popped text
+  push();
+  textStyle(BOLD);
+  textSize(16);
+  fill(255, 255, 255);
+  text(`BUBBLES POPPED: ${bubblesPopped}`, width / 50, height / 1.05);
+  pop();
 
   if (predictions.length > 0) {
     let hand = predictions[0];
@@ -71,15 +90,38 @@ function draw() {
     pop();
     // Check bubble popping
     let d = dist(tipX, tipY, bubble.x, bubble.y);
-    if (d < bubble.size/2) {
-      bubble.x = random(width);
-      bubble.y = height;
+    if (d < bubble.size / 2) {
+      bubble.scared = true;
+      bubble.touch = true;
+      bubble.x += random(1);
+      bubble.y += random(1);
+      bubblesPopped += 1;
     }
+  }
+
+  if (bubble.vx > 0) {
+    bubble.angle = atan2(bubble.vy, bubble.vx);
+  }
+  if (bubble.vx < 0) {
+    bubble.angle = atan2(bubble.vy, bubble.vx);
+  }
+
+  if (bubble.scared === true) {
+    bubble.vx = bubble.vxRunning;
+    bubble.vy = bubble.vyRunning;
   }
 
   // Move the bubble
   bubble.x += bubble.vx;
   bubble.y += bubble.vy;
+
+  //Bouncing
+  if (bubble.x > width - bubble.r || bubble.x < bubble.r) {
+    bubble.vx = -bubble.vx;
+  }
+  if (bubble.y > height - bubble.r || bubble.y < bubble.r) {
+    bubble.vy = -bubble.vy;
+  }
 
   if (bubble.y < 0) {
     bubble.x = random(width);
@@ -87,8 +129,8 @@ function draw() {
   }
 
   push();
-  fill(0, 100, 200);
-  noStroke();
-  ellipse(bubble.x, bubble.y, bubble.size);
+  imageMode(CENTER);
+  image(sheep, bubble.x, bubble.y);
+  rotate(sheep.angle);
   pop();
 }
