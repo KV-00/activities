527fcadc1e15a928652703300638c2b325cb1062
KV-00 <vaillancourtkeven@gmail.com>
2022-02-10 16:13:08 -0500

    working on it

--- bubble-popper-plus-plus/js/script.js
+++ bubble-popper-plus-plus/js/script.js
@@ -2,11 +2,7 @@
 
 // Have sheep stop adding to boop count once they run away
 
-// Have the player start the game by touching the start message
-
 // Have more sheeps spawn in once all sheep are off the field
-
-// Have sheep's angles work correctly
 
 // The user's webcam
 let video = undefined;
@@ -23,6 +19,9 @@ let bg;
 // Sounds
 let bleet;
 let bgmusic;
+
+let tipX = 0;
+let tipY = 0;
 
 let gameState = "start";
 
@@ -77,13 +76,70 @@ function setup() {
 function draw() {
   image(bg, 0, 0, width, height);
 
+  if (gameState === `start`) {
+    startScreen();
+  } else if (gameState === `play`) {
+    playScreen();
+  }
+}
+
+function startScreen() {
+  handlePin();
+
+  textStyle(BOLD);
+  textSize(48);
+  fill(255, 255, 255);
+  text("SHEEP BOOPER PRO", width / 10, height / 2);
+
+  fill(0);
+  rect(width / 4, height / 1.5, 292, -24);
+
+  textStyle(BOLD);
+  textSize(24);
+  fill(255, 255, 255);
+  text("*BOOP* HERE TO BEGIN!", width / 4, height / 1.5);
+
+  // Really it's better to make the position of this "button" as variables
+  // rather than typing out the position explicitly like this!
+
+  let d = dist(tipX, tipY, width / 4, height / 1.5);
+  if (d < 100) {
+    gameState = `play`;
+  }
+}
+
+function playScreen() {
+  handlePin();
+
+  // Check bubble popping
+  let d = dist(tipX, tipY, bubble.x, bubble.y);
+  if (d < bubble.size / 2) {
+    bubble.scared = true;
+    bubble.touch = true;
+    bubble.x += random(1);
+    bubble.y += random(1);
+    sheepsBooped += 1;
+    bleet.play();
+  }
+
+  // Sheep sprite -> function
+  displaySheep();
+
+  // Bubbles popped text -> function
+  displayUI();
+
+  // Sheep scaring -> moveSheep
+  moveSheep();
+}
+
+function handlePin() {
   if (predictions.length > 0) {
     let hand = predictions[0];
     let index = hand.annotations.indexFinger;
     let tip = index[3];
     let base = index[0];
-    let tipX = tip[0];
-    let tipY = tip[1];
+    tipX = tip[0];
+    tipY = tip[1];
     let baseX = base[0];
     let baseY = base[1];
 
@@ -101,98 +157,56 @@ function draw() {
     fill(255, 255, 255);
     ellipse(tipX, tipY, 20);
     pop();
-
-    // Check bubble popping
-    let d = dist(tipX, tipY, bubble.x, bubble.y);
-    if (d < bubble.size / 2) {
-      bubble.scared = true;
-      bubble.touch = true;
-      bubble.x += random(1);
-      bubble.y += random(1);
-      sheepsBooped += 1;
-      bleet.play();
-    }
   }
+}
 
-  // Sheep sprite
+function displaySheep() {
   push();
   imageMode(CENTER);
-  image(sheep, bubble.x, bubble.y);
-  rotate(bubble.angle);
+  translate(bubble.x, bubble.y); // NEW!
+  rotate(bubble.angle); // NEW (moved up)
+  image(sheep, 0, 0);
   pop();
-
-  // Start text
-  if (gameState == "start") {
-    textStyle(BOLD);
-    textSize(48);
-    fill(255, 255, 255);
-    text("SHEEP BOOPER PRO", width / 10, height / 2);
-
-    textStyle(BOLD);
-    textSize(24);
-    fill(255, 255, 255);
-    text("*BOOP* HERE TO BEGIN!", width / 4, height / 1.5);
-  }
-
-  if (gameState == "play") {
-    // Bubbles popped text
-    push();
-    textStyle(BOLD);
-    textSize(16);
-    fill(255, 255, 255);
-    text(`SHEEPS BOOPED: ${sheepsBooped}`, width / 50, height / 1.05);
-    pop();
-
-    // Sheep scaring
-    if (bubble.scared === true) {
-      bubble.vx = bubble.vxRunning;
-      bubble.vy = bubble.vyRunning;
-    }
-
-    // Move the bubble
-    bubble.x += bubble.vx;
-    bubble.y += bubble.vy;
-
-    //Bouncing
-    if (
-      (bubble.x > width - bubble.r && !bubble.touch) ||
-      (bubble.x < bubble.r && !bubble.touch)
-    ) {
-      bubble.vx = -bubble.vx;
-    }
-    if (
-      (bubble.y > height - bubble.r && !bubble.touch) ||
-      (bubble.y < bubble.r && !bubble.touch)
-    ) {
-      bubble.vy = -bubble.vy;
-    }
-
-    // Facing direction of movement
-    if (bubble.vx > 0) {
-      bubble.angle = atan2(bubble.vy, bubble.vx);
-    }
-    if (bubble.vx < 0) {
-      bubble.angle = atan2(bubble.vy, bubble.vx);
-    }
-  }
 }
 
-function sceneManager() {
-  if (gameState == "start") {
-    startScreen();
-  }
-  if (gameState == "play") {
-    playScreen();
-  }
+function displayUI() {
+  push();
+  textStyle(BOLD);
+  textSize(16);
+  fill(255, 255, 255);
+  text(`SHEEPS BOOPED: ${sheepsBooped}`, width / 50, height / 1.05);
+  pop();
 }
 
-function playScreen() {}
+function moveSheep() {
+  if (bubble.scared === true) {
+    bubble.vx = bubble.vxRunning;
+    bubble.vy = bubble.vyRunning;
+  }
 
-function startScreen() {
-  rect(width / 2, height / 2, 100, 100);
-  preload();
-}
+  // Move the bubble
+  bubble.x += bubble.vx;
+  bubble.y += bubble.vy;
 
-function mousePressed() {
-  gameState = "play";
+  //Bouncing
+  if (
+    (bubble.x > width - bubble.r && !bubble.touch) ||
+    (bubble.x < bubble.r && !bubble.touch)
+  ) {
+    bubble.vx = -bubble.vx;
+  }
+  if (
+    (bubble.y > height - bubble.r && !bubble.touch) ||
+    (bubble.y < bubble.r && !bubble.touch)
+  ) {
+    bubble.vy = -bubble.vy;
+  }
+
+  // Facing direction of movement
+  if (bubble.vx > 0) {
+    bubble.angle = atan2(bubble.vy, bubble.vx);
+  }
+  if (bubble.vx < 0) {
+    bubble.angle = atan2(bubble.vy, bubble.vx);
+  }
 }
