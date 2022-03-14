78fa4e8207e639ed61a4ac5e2cedeb0c1eb95523
KV-00 <vaillancourtkeven@gmail.com>
2022-03-14 13:50:43 -0400

    finished raving redactionist++

--- raving-redactionist-plus-plus/js/script.js
+++ raving-redactionist-plus-plus/js/script.js
@@ -1,23 +1,58 @@
 "use strict";
 
 let r = undefined;
+let frequency = 0.001;
+let score = 0;
+let lives = 5;
 
-$(`.top-secret`).on(`click`, redact);
-setInterval(revelation, 500);
+let overStayed = setInterval(Appearance, 500);
 
-function redact(event) {
-  $(this).removeClass(`revealed`);
-  $(this).addClass(`redacted`);
+document.getElementById("score").innerHTML = score;
+document.getElementById("lives").innerHTML = lives;
+
+function bonk(event) {
+  $(this).removeClass(`appear`);
+  $(this).addClass(`empty`);
+  $(this).off(`click`);
+  $(this).html(`___`);
+
+  frequency *= 1.01;
+  score += 1;
+
+  document.getElementById("score").innerHTML = score;
 }
 
-function revelation() {
-  $(`.redacted`).each(attemptReveal);
+function Appearance() {
+  console.log("Interval is still going");
+  $(`.empty`).each(attemptAppear);
 }
 
-function attemptReveal() {
+function attemptAppear() {
   r = Math.random();
-  if (r < 0.1) {
-    $(this).removeClass(`redacted`);
-    $(this).addClass(`revealed`);
+  if (r < frequency) {
+    $(this).removeClass(`empty`);
+    $(this).addClass(`appear`);
+    $(this).on(`click`, bonk);
+
+    $(this).html(`ʕ•ᴥ•ʔ`);
+    let self = $(this);
+
+    setTimeout(function () {
+      if (self.hasClass(`appear`)) {
+        self.off(`click`);
+        self.addClass(`overStay`);
+
+        if (lives > 1) {
+          lives -= 1;
+          document.getElementById("lives").innerHTML = lives;
+        } else if (lives > 0) {
+          $(`.hole`).off(`click`);
+          $(`.hole`).addClass(`overStay`);
+          clearInterval(overStayed);
+          lives -= 1;
+          document.getElementById("lives").innerHTML = lives;
+        }
+      }
+    }, 5000);
   }
 }
