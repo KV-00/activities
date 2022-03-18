1ae13a6a6cb4e1cd6e0562b4b921d07b8e4557f3
KV-00 <vaillancourtkeven@gmail.com>
2022-03-18 18:04:22 -0400

    finished code taker

--- code-taker/js/script.js
+++ code-taker/js/script.js
@@ -1,11 +1,33 @@
-/**
-Title of Project
-Author Name
-
-This is a template. You must fill in the title,
-author, and this description to match your project!
-*/
-
 "use strict";
 
-// Code goes here
+let secretAnswer = "Theremin";
+let classChangeDuration = 500;
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function closeDialog() {
+      $(this).dialog(`close`);
+    },
+  },
+});
+
+$(`.secret`).one(`mouseover`, function foundLetter(event) {
+  $(this).addClass(`found`, classChangeDuration);
+  $(this).draggable({
+    helper: `clone`,
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function dropLetter(event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    //Check if they get it
+    if ($(this).text() === secretAnswer) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  },
+});
